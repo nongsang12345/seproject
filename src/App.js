03512fa3688eba6ee574ba2a344bef6a93e175bc
sang <sangkungbibi@gmail.com>
2024-02-02 00:36:14 +0700

    sang importcoursefirst
    sang importcoursefirst

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { GoogleLogin } from 'react-google-login'
-import { gapi } from 'gapi-script'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react';
+import Importcourse from './Importcourse';
 
-  const clientId = "989605622776-ue6e395jnkt84309ub3a5b83pg7svq28.apps.googleusercontent.com"
-  
-  useEffect(() => {
-    const initClient = () => {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ''
-      })
-    }
-    gapi.load("client:auth2",initClient)
-
-  }, [])
-
-  const onSuccess = (res) => {
-    console.log('success',res)
-  }
-
-  const onFailure = (res) => {
-    console.log('failed',res)
-  }
-
-
+const App = () => {
   return (
-    <GoogleLogin
-      clientId={clientId}
-      buttonText="Sight in with Google"
-      onSuccess={onSuccess}
-      onFailure={onFailure}
-      cookiePolicy={'single_host_origin'}
-      isSignedIn={true}
-    />
-      
-  )
-}
+    <div>
+      <Importcourse />
+      {/* The rest of your website content goes here */}
+    </div>
+  );
+};
 
-export default App
+export default App;
